eb256025545596e5d0c16f5d4c534e7b44dfeda3
lyb <lyb@ins.com>
2016-08-03 10:05:18 +0800

    目录优化

---
+++ tcp/connection management/tcp12/tcpclient.c
@@ -0,0 +1,57 @@
+#include "../common/common.h"
+
+int main(int argc, char **argv)
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char writebuf[TRANSSIZE];   
+    int mssopt, wsopt;
+    
+    if (argc !=4){
+        printf("argc error:ip mssopt wsopt\n");
+        exit(1);
+    }
+    
+    mssopt = atoi(argv[2]);
+    wsopt = atoi(argv[3]);
+    snprintf(writebuf,TRANSSIZE,"hello");
+    
+    sockfd = socket(AF_INET,SOCK_STREAM,0);
+
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+    inet_pton(AF_INET,argv[1],&servaddr.sin_addr);
+    
+    Setsockopt(sockfd, SOL_TCP, TCP_MAXSEG,&mssopt, sizeof mssopt);
+        
+    Setsockopt(sockfd, SOL_SOCKET, SO_RCVBUF,&wsopt, sizeof wsopt);
+
+    Connect(sockfd,(SA*)&servaddr,sizeof(servaddr));
+
+    Write(sockfd,writebuf,strlen(writebuf)+1);
+
+    sleep(10);
+
+    Close(sockfd);
+
+
+    return 0;
+
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tcp/connection management/tcp12/tcpserver.c
@@ -0,0 +1,36 @@
+#include "../common/common.h"
+
+int main()
+{
+    int Listenfd,connfd;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+    char writebuf[TRANSSIZE];
+    
+    snprintf(writebuf,TRANSSIZE,"world");
+
+    Listenfd = Socket(AF_INET,SOCK_STREAM,0);
+
+    memset(&servaddr,0,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port =  htons(SERV_PORT);
+    
+    Bind(Listenfd,(SA*)&servaddr,sizeof(servaddr));
+
+    Listen(Listenfd,LISTENQ);
+    
+    for( ; ;){
+        clilen = sizeof(cliaddr);
+        connfd = Accept(Listenfd,(SA*)&cliaddr,&clilen);
+        
+        sleep(3);
+        Write(connfd,writebuf,strlen(writebuf)+1);
+
+        sleep(5);
+        Close(connfd);
+    }
+    return 0;
+}
+    
+
