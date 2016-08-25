a34297551d4e7ecfe3254b766224d445abb21559
lyb <lyb@ins.com>
2016-08-25 09:17:13 +0800

    RTO timer&fack

--- tcp/retrans/tcp15/tcpserver.c
+++ tcp/retrans/tcp15/tcpserver.c
@@ -1,18 +1,19 @@
+
 #include "../../common/common.h"
 #include "../../common/rawops.h"
 #include "../../common/tcp_private.h"
 
+
+
 int main()
 {
     int Listenfd,connfd;
-    int len,val,last_in = 0,last_out = 0,i=0;
-    struct tcp_info_user info;
     struct timespec;
     socklen_t clilen;
     struct sockaddr_in cliaddr, servaddr;
-    char writebuf[TRANSSIZE];
-    
-    snprintf(writebuf,TRANSSIZE,"hello");
+    int val,len,i,last_in = 0,last_out = 0;
+    struct tcp_info_user info;
+    char writebuf[MAX_PKT_SIZE];
 
     Listenfd = Socket(AF_INET,SOCK_STREAM,0);
 
@@ -22,12 +23,8 @@ int main()
     servaddr.sin_port =  htons(SERV_PORT);
     
     Bind(Listenfd,(SA*)&servaddr,sizeof(servaddr));
-
     Listen(Listenfd,LISTENQ);
-    
-    len = sizeof(int);
-    Getsockopt(Listenfd, SOL_TCP, TCP_USER_TIMEOUT,(void *)&val, (socklen_t *)&len);
-	printf("user_timeout:%d\n",val);
+
     for( ; ;){
         clilen = sizeof(cliaddr);
         connfd = Accept(Listenfd,(SA*)&cliaddr,&clilen);
@@ -38,22 +35,34 @@ int main()
         len = sizeof(int);
         Setsockopt(connfd, SOL_TCP, TCP_NODELAY,(void *)&val, len);
         
-        len = sizeof(info);
-        Getsockopt(connfd, SOL_TCP, TCP_INFO,(void *)&info, (socklen_t *)&len);
-        printf("before write rto:%u, retrans:%u,rtt:%u\n",info.tcpi_rto,info.tcpi_retransmits,info.tcpi_rtt);
         
+        snprintf(writebuf,TRANSSIZE,"world01");
         Write(connfd,writebuf,strlen(writebuf)+1);
         
-        snprintf(writebuf,TRANSSIZE,"world");
+        sleep_ms(20);
+        snprintf(writebuf,TRANSSIZE,"world02");
         Write(connfd,writebuf,strlen(writebuf)+1);
-        printf("serv write hello  and  world i:%d\n",i);;
+        
+        sleep_ms(20);
+        snprintf(writebuf,TRANSSIZE,"world03");
+        Write(connfd,writebuf,strlen(writebuf)+1);
+        
+        
+        sleep_ms(10);
+        snprintf(writebuf,TRANSSIZE,"world04");
+        Write(connfd,writebuf,strlen(writebuf)+1);
+        
+        
+        sleep_ms(10);
+        snprintf(writebuf,TRANSSIZE,"world05");
+        Write(connfd,writebuf,strlen(writebuf)+1);;
+        
         
         i = 0;
         while(i < 100*200)
         {
             len = sizeof(info);
             Getsockopt(connfd, SOL_TCP, TCP_INFO,(void *)&info, (socklen_t *)&len);
-            
             if( (last_in != info.tcpi_segs_in) || (last_out != info.tcpi_segs_out) )
             {
                 sleep_ms(1);
@@ -70,18 +79,11 @@ int main()
             
             sleep_ms(10);
             i++;
-            
         }
-
-
-        //sleep(50);
-        len = sizeof(int);
-        Getsockopt(connfd, SOL_SOCKET, SO_ERROR, (void *)&val, (socklen_t *)&len);
-        if(val > 0)
-        {
-            printf("%s\n",strerror(val));
-        }
-        printf("before close\n");
+        
+        sleep(200);
+        
+        printf("close\n");
         Close(connfd);
     }
     return 0;
--- tcp/retrans/tcp15/testraw.c
+++ tcp/retrans/tcp15/testraw.c
@@ -2,6 +2,8 @@
 #include "../../common/rawops.h"
 #include <pthread.h>  
 
+
+
 void *recv_function(void *arg); 
 void *send_function(void *arg); 
 
@@ -10,6 +12,7 @@ void *send_function(void *arg);
 void *send_function(void *arg)
 {
     int sockfd, tot_len;
+    //u16 lostseq1;//,lostseq2,lostseq3,lostseq4;
     unsigned char buffer[MAX_PKT_SIZE] = {"hello\n\0"};
     
     sockfd = *( (int*)arg );
@@ -17,9 +20,10 @@ void *send_function(void *arg)
     //处理数据发送和业务逻辑
     tot_len = builddatapkt(buffer, recvacknumber,  strlen((const char *)buffer));
     rawsend(sockfd, buffer, tot_len);
-    //sendflag = 1;
+    sleep(1);
     
-    sleep(250);
+    
+    sleep(300);
     
     return 0;
 }
@@ -28,40 +32,62 @@ void *send_function(void *arg)
 void *recv_function(void *arg)
 {
 
-    //u32 lastacknumber;
-    int sockfd, tot_len,i = 0;
+    int sockfd, tot_len, i=0;
     u16 recvlen;
+    //u32 flag;
     unsigned char buffer[MAX_PKT_SIZE];
     
     //接收线程detach自己
     pthread_detach(pthread_self());
     
     sockfd = *( (int*)arg );
-    //负责处理接收及ack回复工作
-    //lastacknumber = recvacknumber;
+
     while(1)
     {       
         //等待接收ACK
-        recvlen = rawrecv(sockfd, buffer, MAX_PKT_SIZE);
+        recvlen = rawadvrecv(sockfd, buffer, MAX_PKT_SIZE,TCP_SACKOPT);
         
         //判断是否收到了需要回复ACK的报文
         if(containdata(buffer, recvlen))
         {
-            
-            if(i == 4)
+             
+            if(i==0)
             {
-                //acknumber发生变化  接收到了data 发送ACK
-                //tot_len = buildackpkt(buffer,recvacknumber-6,TCP_TSOPT);
+                senddelay = 500;
                 tot_len = buildackpkt(buffer,recvacknumber,TCP_TSOPT);
-                //回复一个ACK报文 以结束第一个TCP报文的重传
+                //回复一个ACK报文 
                 rawsend(sockfd,buffer,tot_len);
-                //lastacknumber = recvacknumber;
+            
+            }  
+            
+
+            
+            if(i==4)
+            {
+                senddelay = 400;
+                recvacknumber = recvacknumber - (i) * 8;
+                resetsackblk();
+                appendsackblk((recvacknumber+ (i-1)*8),(recvacknumber+(i)*8));
+                tot_len = buildackpkt(buffer,recvacknumber,TCP_TSOPT|TCP_SACKOPT);
+                //tot_len = buildackpkt(buffer,recvacknumber,TCP_TSOPT);
+                //回复一个ACK报文 
+                rawsend(sockfd,buffer,tot_len);
+            
+            }             
+                      
+            
+            if(i > 7)
+            {
+                senddelay = 500;
+                tot_len = buildackpkt(buffer,recvacknumber,TCP_TSOPT);
+                //回复一个ACK报文 
+                rawsend(sockfd,buffer,tot_len);
             }
+            
 
             i++;
-        }
-        
-        
+
+        } 
     
     }
 
@@ -87,7 +113,7 @@ int main(int argc, char **argv)
     
     initrawops(sockfd);
     rawconnect(sockfd);
-    //sleep(5);
+    //sleep(500);
     
 
     res = pthread_create(&recv_thread, NULL, recv_function, (void *)(&sockfd));  
